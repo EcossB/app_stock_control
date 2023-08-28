def2bf85b726c5a2e6aaa617dc498f28cfa6b0d1
EcossB <erwingcoss21@hotmail.com>
2023-08-28 12:40:46 -0400

    adding a new construct and getter setter of categoriaID

--- src/main/java/com/alura/jdbc/modelo/Producto.java
+++ src/main/java/com/alura/jdbc/modelo/Producto.java
@@ -9,6 +9,7 @@ public class Producto {
     private String nombre;
     private String descripcion;
     private Integer cantidad;
+    private Integer categoriaId;
 
     public Producto(String nombre, String descripcion, Integer cantidad) {
         this.nombre = nombre;
@@ -23,6 +24,11 @@ public class Producto {
         this.cantidad = cantidad;
     }
 
+    public Producto(int id, String nombre, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
 
 
     public String getNombre() {
@@ -57,10 +63,19 @@ public class Producto {
         return this.id;
     }
 
+    public void setCategoriaId(Integer categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+
     @Override
     public String toString() {
         return String.format(
                 "{id: %s, nombre: %s, descripcion: %s, cantidad: %d}"
         , this.id, this.nombre, this.descripcion, this.cantidad);
+    }
+
+
+    public Integer getCategoriaId() {
+        return this.categoriaId;
     }
 }
